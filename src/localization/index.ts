78ebceaa3746a3c4f4952dd0bcf1a8d12c2656e4
Lexpeartha <sevic.official@gmail.com>
2021-04-24 11:22:02 +0200

    Improve readability of locale utils

--- src/localization/index.ts
+++ src/localization/index.ts
@@ -1,17 +1,19 @@
 import { createI18n } from "vue-i18n";
 
 import { supportedLocale } from "@/types";
-import { checkLocalStorageForLocale } from "@/utils/locale";
+import { checkForLocale } from "@/utils/locale";
 
 import languages from "./languages.json";
 
-const preferedLocale: supportedLocale = checkLocalStorageForLocale();
+const preferedLocale: supportedLocale = checkForLocale();
+const fallbackLocale = "en";
 
 const messages = languages;
 
 const i18n = createI18n({
+  legacy: false,
   locale: preferedLocale,
-  fallbackLocale: "en",
+  fallbackLocale: fallbackLocale,
   messages,
 });
 
--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -1,8 +1,10 @@
+import { useI18n } from "vue-i18n";
 import { supportedLocale } from "@/types";
 
 const localeAccessName = "locale";
 
 const checkLocalStorageForLocale = (): supportedLocale => {
+  // Gets either the locale from the local storage or fallback "en"
   return (localStorage.getItem(localeAccessName) as supportedLocale) || "en";
 };
 
@@ -10,4 +12,15 @@ const setLocalStorageLocale = (newLocale: supportedLocale): void => {
   return localStorage.setItem(localeAccessName, newLocale);
 };
 
-export { checkLocalStorageForLocale, setLocalStorageLocale };
+const updateLocale = (newLocale: supportedLocale): void => {
+  setLocalStorageLocale(newLocale);
+  // Initiates & updates global i18n object's locale
+  const i18n = useI18n();
+  i18n.locale.value = newLocale;
+};
+
+const checkForLocale = (): supportedLocale => {
+  return checkLocalStorageForLocale();
+};
+
+export { checkForLocale, updateLocale };
