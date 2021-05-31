5964999d21af84569b94dd9b6a73f3e1e2f847f3
Lexpeartha <sevic.official@gmail.com>
2021-05-31 14:45:48 +0200

    Add no properties found content

--- src/components/home/CityProperties.vue
+++ src/components/home/CityProperties.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="title-section ion-padding">
-    <ion-label class="section-title">
-      <strong> {{ textData.title }} </strong>
-    </ion-label>
-    <a @click="cancelSearch" class="section-link">
-      Cancel search
-    </a>
+  <div v-if="propertiesByLocation.length">
+    <div class="title-section ion-padding">
+      <ion-label class="section-title">
+        <strong> {{ textData.title }} </strong>
+      </ion-label>
+      <a @click="cancelSearch" class="section-link">
+        Cancel search
+      </a>
+    </div>
+    <div class="property-list ion-padding-start">
+      <property-card
+        v-for="(property, $index) in propertiesByLocation"
+        v-bind="property"
+        :key="$index"
+      ></property-card>
+    </div>
   </div>
-  <div class="property-list ion-padding-start">
-    <property-card
-      v-for="(property, $index) in propertiesByLocation"
-      v-bind="property"
-      :key="$index"
-    ></property-card>
+  <div v-else class="properties-not-found ion-padding">
+    <div class="title-section ion-padding-vertical">
+      <ion-label class="section-title">
+        <strong> {{ textData.notFound.title }} </strong>
+      </ion-label>
+    </div>
+    <ion-text class="not-found-text ion-margin-vertical">
+      {{ textData.notFound.description }}
+    </ion-text>
+    <ion-button
+      class="not-found-button ion-margin-vertical"
+      @click="cancelSearch"
+    >
+      {{ textData.notFound.button }}
+    </ion-button>
   </div>
 </template>
 
@@ -20,7 +38,7 @@
   import { defineComponent, computed, watchEffect } from "vue";
   import { useStore } from "vuex";
   import { useI18n } from "vue-i18n";
-  import { IonLabel } from "@ionic/vue";
+  import { IonLabel, IonButton, IonText } from "@ionic/vue";
 
   import PropertyCard from "@/components/ui/PropertyCardVertical.vue";
 
@@ -37,6 +55,8 @@
     components: {
       PropertyCard,
       IonLabel,
+      IonButton,
+      IonText,
     },
     setup(props, { emit }) {
       const store = useStore();
@@ -51,13 +71,14 @@
         title: t("components.property.searchPropertyTitle", {
           city: props.location,
         }),
+        notFound: {
+          title: t("components.property.notFoundTitle", {
+            city: props.location,
+          }),
+          description: t("components.property.notFoundDescription"),
+          button: t("components.property.goBack"),
+        },
       };
-
-      watchEffect(() => {
-        textData.title = t("components.property.searchPropertyTitle", {
-          city: props.location,
-        });
-      });
 
       const cancelSearch = () => {
         emit("cancelSearch");
@@ -72,4 +93,8 @@
   });
 </script>
 
-<style scoped></style>
+<style scoped>
+  .properties-not-found {
+    text-align: left;
+  }
+</style>
